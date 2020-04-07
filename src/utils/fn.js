39fd687fc4ef51f9611203a07ea917d9e430e757
江丽康 <22652@etransfar.com>
2020-04-07 10:11:40 +0800

    fn函数集

---
+++ src/utils/fn.js
@@ -0,0 +1,56 @@
+// 随机数函数
+export const random = (min, max) => {
+  return min + Math.ceil(Math.random() * (max - min));
+}
+
+// 日期判断函数
+export const isDuringDate = (beginDateStr, endDateStr) => {
+  var curDate = new Date(),
+      beginDate = new Date(beginDateStr),
+      endDate = new Date(endDateStr);
+  if (curDate >= beginDate && curDate <= endDate) {
+      return true;
+  }
+  return false;
+}
+// isDuringDate('2020/05/01', '2020/05/08') == false
+
+// 时间戳转时间函数
+export const timestampToTime = timestamp => {
+  var date = new Date(timestamp);
+
+  var Y = date.getFullYear() + '/';
+  var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '/';
+  var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
+  var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+  var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+  var s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
+  return Y + M + D + h + m + s;
+}
+
+// 随机时间
+export const randomTime = (min,max) => {
+  //min,max为时间戳
+  const random = min + Math.ceil(Math.random() * (max - min));
+  const newStamp = new Date().getTime() - random
+  var date = new Date(newStamp);
+
+  var Y = date.getFullYear() + '/';
+  var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '/';
+  var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
+  var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+  var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+  var s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
+  return Y + M + D + h + m + s;
+}
+
+//洗牌函数
+export const shuffle = (arr) => {
+  　　for (var i = arr.length - 1; i >= 0; i--) {
+  　　　　var randomIndex = Math.floor(Math.random() * (i + 1));
+  　　　　var itemAtIndex = arr[randomIndex];
+  　　　　arr[randomIndex] = arr[i];
+  　　　　arr[i] = itemAtIndex;
+  　　}
+  　　return arr;
+}
