b28c2890c8e231d2ceb831805ae2b4be8ab49eda
江丽康 <22652@etransfar.com>
2020-04-09 20:39:37 +0800

    节流防抖

--- src/utils/fn.js
+++ src/utils/fn.js
@@ -66,3 +66,27 @@ export const shuffle = (arr) => {
   　　}
   　　return arr;
 }
+
+// 防抖
+function debounce(fn, wait) {    
+  var timeout = null;    
+  return function() {        
+      if(timeout !== null)   clearTimeout(timeout);        
+      timeout = setTimeout(fn, wait);    
+  }
+}
+
+// 节流
+function throttle (func, delay) {            
+  var timer = null;            
+  return function() {                
+      var context = this;               
+      var args = arguments;                
+      if (!timer) {                    
+          timer = setTimeout(function() {                        
+              func.apply(context, args);                        
+              timer = null;                    
+          }, delay);                
+      }            
+  }        
+}   
